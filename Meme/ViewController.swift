c4df910f1a21ae512b8a888fcb4f8527289f7bd3
Victor Luiz <victor_luizsantos@hotmail.com>
2022-03-07 17:18:35 -0300

    Adicionado o comentário da Mari

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // remover seguimentos
+        //Oi,tudobem
         
         topSegControl.removeAllSegments()
         
